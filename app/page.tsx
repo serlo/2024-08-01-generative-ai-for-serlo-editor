9b77282ad140ac7393e38813af0e9ecadd7630ca
Stephan Kulla <git.mail@kulla.me>
2024-08-01 21:08:55 +0200

    feat: Add basic elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,70 @@
-import { Card, Flex, Theme } from '@radix-ui/themes'
+'use client'
+
+import { Button, Card, Flex, Heading, Theme } from '@radix-ui/themes'
+import * as Form from '@radix-ui/react-form'
 import '@radix-ui/themes/styles.css'
+import { SerloEditor, SerloEditorProps, SerloRenderer } from '@serlo/editor'
+import React from 'react'
+
+const initialState = {
+  plugin: 'rows',
+  state: [{ plugin: 'text' }],
+}
 
 export default function Home() {
+  const [inputContent, setInputContent] = React.useState<Content>(initialState)
+  const [outputContent, setOutputContent] =
+    React.useState<Content>(initialState)
+  const [prompt, setPrompt] = React.useState('Vereinfache den Text')
+
+  const generateContent = async () => {
+    setOutputContent(inputContent)
+  }
+
   return (
     <Theme>
       <main className="p-5">
-        <Flex gap="3">
-          <FlexItem>Serlo Editor</FlexItem>
-          <FlexItem>Settings</FlexItem>
-          <FlexItem>Final Editor</FlexItem>
+        <Flex gap="3" wrap="wrap">
+          <FlexItem>
+            <Heading>Input for the generative AI</Heading>
+            <SerloEditor
+              initialState={inputContent}
+              onChange={({ changed, getDocument }) => {
+                if (changed) {
+                  const newState = getDocument()
+
+                  if (newState != null) {
+                    setInputContent(newState)
+                  }
+                }
+              }}
+            >
+              {(editor) => {
+                return <>{editor.element}</>
+              }}
+            </SerloEditor>
+          </FlexItem>
+          <FlexItem>
+            <Heading>Settings for the generative AI</Heading>
+            <Form.Root>
+              <Form.Field name="prompt">
+                <Form.Label>User Prompt:</Form.Label>
+                <Form.Control asChild>
+                  <textarea
+                    className="w-full p-2 border border-gray-300 rounded-md"
+                    rows={8}
+                    value={prompt}
+                    onChange={(event) => setPrompt(event.target.value)}
+                  />
+                </Form.Control>
+              </Form.Field>
+            </Form.Root>
+            <Button onClick={generateContent}>Generate Content with AI</Button>
+          </FlexItem>
+          <FlexItem>
+            <Heading>Output of the generative AI</Heading>
+            <SerloRenderer document={outputContent} />
+          </FlexItem>
         </Flex>
       </main>
     </Theme>
@@ -16,5 +72,7 @@ export default function Home() {
 }
 
 function FlexItem({ children }: { children: React.ReactNode }) {
-  return <Card className="min-w-80 w-1/3">{children}</Card>
+  return <Card className="min-w-[600px] w-1/3">{children}</Card>
 }
+
+type Content = SerloEditorProps['initialState']
